14af6101b949811dcd7eca159a3b2138556cc942
Taylor Bisset <taylorbisset@gmail.com>
2024-10-07 13:36:26 -0700

    Begin work on MainMenuScript.cs

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenuScript : MonoBehaviour
 {
+    public GameObject mainMenu; // References the MainMenuObject
+    public GameObject levelSelectMenu;  // References the LevelSelectMenuObject
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     public void QuitGame()
     {
-        Application.Quit();
+        Application.Quit(); // Exits the game
     }
 }
